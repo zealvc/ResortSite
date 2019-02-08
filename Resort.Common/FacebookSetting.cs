e2a541d59a2041b2bd61fbd21dac71547d67f10d
= <=>
2019-02-08 10:27:28 +0700

    Instagram and Facebook Post API

--- Resort.Common/FacebookSetting.cs
+++ Resort.Common/FacebookSetting.cs
@@ -6,7 +6,7 @@ namespace Resort.Common
 {
     public class FacebookSetting
     {
-        public static string AccessToken = "EAAFpoAAdfGgBAH46ZAsTjIZB6Foj6SYx2Tp2fM4ZCqNCisZBxFB0hYrNZCYNWv0Q9gyJZB31RjzzRhcQOv5f3Ci0lM6FRi68RwgFVnmwCRutITZA4MjQ1NZCNoWs06tFd5hGnHnsYcSOOdukmo3NB2lZBJvCGy9n6g38sQ4w3DbCdcAZDZD";
-        public static string PageId = "1206742549473077";
+        public static string AccessToken = "Your_page access_Token";
+        public static string PageId = "Your_page_ID";
     }
 }
