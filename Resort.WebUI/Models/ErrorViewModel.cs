72c57a1f7fe44fbc49723503b7f2b4c71770072e
Dibee <pensereyotdom15@kit.edu.kh>
2019-02-14 10:39:31 +0700

    Update all API of Oauth2, Authentication
    Login/Signup Facebook
    Login/Signup Google
    Login
    Logout
    Register
    forgetpassword
    Email verification

---
+++ Resort.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Resort.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
